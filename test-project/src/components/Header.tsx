0c104ee845b1b01bbf8b6fe62cc8a60114ee30a4
kritical0613 <kritical0613@outlook.com>
2022-03-08 00:25:13 -0600

    feat: complete 3 pages

--- test-project/src/components/Header.tsx
+++ test-project/src/components/Header.tsx
@@ -1,7 +1,44 @@
+import { Container, Nav, Navbar, NavLink, Offcanvas } from "react-bootstrap"
+
+const HeaderNav = () => {
+  return (
+    <Nav className="flex gap-3">
+      <NavLink href="/" className="nav-link">
+        Home
+      </NavLink>
+      <NavLink href="/about" className="nav-link">
+        About
+      </NavLink>
+      <NavLink href="/contact" className="nav-link">
+        Contact Us
+      </NavLink>
+    </Nav>
+  )
+}
+
 const Header = () => {
   return (
-    <>
-    </>
+    <Navbar collapseOnSelect className="navbar navbar-dark bg-dark" expand="sm" sticky="top">
+      <Container>
+        <Navbar.Brand href="/">William's Test Project</Navbar.Brand>
+        <Navbar.Toggle aria-controls="offcanvasNavbar" />
+        <Navbar.Offcanvas
+          aria-labelledby="offcanvasNavbarLabel"
+          placement="end"
+        >
+          <Offcanvas.Header closeButton className="bg-dark" closeVariant="white"></Offcanvas.Header>
+          <Offcanvas.Body className="navbar-dark bg-dark">
+            <HeaderNav />
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
+        <Navbar.Collapse
+          className="justify-content-center"
+          in={false}
+        >
+          <HeaderNav />
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   )
 }
 
--- test-project/src/pages/Contact.tsx
+++ test-project/src/pages/Contact.tsx
@@ -1,6 +1,44 @@
+import { Form, Row, Col, Button, Container } from "react-bootstrap"
+
 const Contact = () => {
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    const data = {
+      name: e.target.name.value,
+      email: e.target.email.value,
+      message: e.target.message.value,
+    }
+
+    console.log(JSON.stringify(data))
+    alert(JSON.stringify(data));
+
+  }
+
   return (
-    <></>
+    <Container className="d-flex align-content-center justify-content-center" style={{ marginTop: '6rem' }}>
+      <Form onSubmit={handleSubmit}>
+        <Row className="mb-3">
+          <Form.Group as={Col} controlId="formGridName">
+            <Form.Label>Name</Form.Label>
+            <Form.Control required name="name" type="text" placeholder="Full Name" />
+          </Form.Group>
+          <Form.Group as={Col} controlId="formGridEmail">
+            <Form.Label>Email</Form.Label>
+            <Form.Control required name="email" type="email" placeholder="Enter email" />
+          </Form.Group>
+        </Row>
+
+        <Form.Group className="mb-3" controlId="formGridMessage">
+          <Form.Label>Message</Form.Label>
+          <Form.Control required name="message" placeholder="Hello, William!" />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Container>
   )
 }
 
